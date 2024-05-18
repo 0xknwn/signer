6df7f3e2d834a0f06406d732f55e3f625f999c28
0xknwn <145777008+0xknwn@users.noreply.github.com>
2024-05-18 23:17:46 +0200

    add a simple /api/version route (#6)

---
+++ _api/version.ts
@@ -0,0 +1,23 @@
+// file: _api/version.ts
+
+// Should run on edge runtime
+export const edge = true;
+
+// Stream the response
+export const streaming = true;
+
+// Enable Incremental Static Regeneration for this endpoint
+export const isr = {
+  expiration: 30,
+};
+
+export default async function handler(request: Request) {
+  const res = await fetch(`${process.env.BACKEND_URL}/version`);
+  const out = await res.json();
+
+  const response = new Response(JSON.stringify({ version: out.version }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+  return response;
+}
