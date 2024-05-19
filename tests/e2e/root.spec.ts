3adb5032f4cbd150410a5bb4ca9b3ae7652ceda0
0xknwn <145777008+0xknwn@users.noreply.github.com>
2024-05-19 12:26:51 +0200

    fix test

--- tests/e2e/root.spec.ts
+++ tests/e2e/root.spec.ts
@@ -7,7 +7,7 @@ test("has title", async ({ page }) => {
   await expect(page).toHaveTitle("signer");
 
   await page.getByRole("link", { name: "signin" }).click();
-  await expect(page).toHaveURL(/\/login$/);
+  await expect(page).toHaveURL(/\/signin$/);
 
   await page.goBack();
   await page.getByRole("link", { name: "signup" }).click();
