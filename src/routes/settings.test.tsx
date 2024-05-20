ef0112ed0732792d3a2d02dd3f25f8eba6558058
0xknwn <145777008+0xknwn@users.noreply.github.com>
2024-05-20 14:54:15 +0200

    remove unused functions

--- src/routes/settings.test.tsx
+++ src/routes/settings.test.tsx
@@ -1,4 +1,4 @@
-import { expect, describe, it } from "vitest";
+import { describe, it } from "vitest";
 
 describe(`settings management`, {}, () => {
   describe(`you did not verify your BIP-39`, {}, () => {
