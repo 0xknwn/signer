9cdb36a8855980ee2fb39e70eb34b46278e79a44
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-01-23 16:10:19 +0100

    configure vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   test: {
     environment: "jsdom",
   },
