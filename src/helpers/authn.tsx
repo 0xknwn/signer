abe897ebe1e16f99c5cca12151f2316651c6704c
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-01-29 07:16:42 +0100

    add and select account

--- src/helpers/authn.tsx
+++ src/helpers/authn.tsx
@@ -2,6 +2,20 @@ import { useContext, createContext } from "react";
 import { useLocation, useNavigate, Navigate } from "react-router";
 import { useState, useEffect } from "react";
 import { decrypt, encrypt } from "./encryption";
+import {
+  classHash as accountClassHash,
+  SmartrAccountABI,
+  accountAddress,
+  classNames as accountClassNames,
+} from "@0xknwn/starknet-modular-account";
+import { CallData } from "starknet";
+import { getKeys } from "./encryption";
+
+export type account = {
+  address: string;
+  name: string;
+  publickey: string;
+};
 
 export const AuthContext = createContext<{
   challenge: string;
@@ -12,6 +26,8 @@ export const AuthContext = createContext<{
   setVerifier: (value: string) => void;
   verify: (key: CryptoKey | null) => Promise<boolean>;
   resetWallet: () => void;
+  getAccounts: () => Promise<account[]>;
+  addAccount: () => Promise<account | null>;
 }>({
   challenge: "",
   verifier: "",
@@ -21,6 +37,8 @@ export const AuthContext = createContext<{
   setVerifier: () => {},
   verify: async () => false,
   resetWallet: () => {},
+  getAccounts: async () => [],
+  addAccount: async () => null,
 });
 
 export const useAuth = () => {
@@ -35,6 +53,7 @@ const store = {
   challenge: "smartr-challenge",
   verifier: "smartr-verifier",
   mnemonic: "smartr-mnemonic",
+  accounts: "smartr-accounts",
 };
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
@@ -60,6 +79,69 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     setStateVerifier(value);
   };
   const [cipher, setCipher] = useState(null as CryptoKey | null);
+
+  const getAccounts = async () => {
+    let accountNumber = 0;
+    const accounts: account[] = [];
+    while (true) {
+      const account = localStorage.getItem(
+        `${store.accounts}/${accountNumber}`
+      );
+      if (!account || account === "") {
+        break;
+      }
+      accounts.push(JSON.parse(account));
+      accountNumber++;
+    }
+    return accounts;
+  };
+
+  const addAccount = async () => {
+    try {
+      const accounts = await getAccounts();
+      const index = accounts.length;
+      const { publicKey } = getKeys(passphrase, index);
+      const starkValidatorClassHash = accountClassHash(
+        accountClassNames.StarkValidator
+      );
+      const calldata = new CallData(SmartrAccountABI).compile("constructor", {
+        core_validator: starkValidatorClassHash,
+        args: [publicKey],
+      });
+      const address = accountAddress(
+        accountClassNames.SmartrAccount,
+        publicKey,
+        calldata
+      );
+      const account = {
+        address,
+        name: `Account #${index}`,
+        publickey: publicKey,
+      };
+      localStorage.setItem(
+        `${store.accounts}/${index}`,
+        JSON.stringify(account)
+      );
+      return account;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  };
+
+  const removeAccounts = async () => {
+    let accountNumber = 0;
+    while (true) {
+      const account = localStorage.getItem(
+        `${store.accounts}/${accountNumber}`
+      );
+      if (!account) {
+        break;
+      }
+      localStorage.removeItem(`${store.accounts}/${accountNumber}`);
+      accountNumber++;
+    }
+  };
 
   const verify = async (key: CryptoKey | null) => {
     if (key) {
@@ -92,6 +174,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     localStorage.removeItem(store.verifier);
     localStorage.removeItem(store.mnemonic);
     setStatePassphrase("");
+    removeAccounts();
     navigate("/");
   }, [verifier]);
 
@@ -123,6 +206,8 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     setVerifier,
     verify,
     resetWallet,
+    getAccounts,
+    addAccount,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
--- src/routes/accounts.tsx
+++ src/routes/accounts.tsx
@@ -1,24 +1,71 @@
 import NavBar from "../components/navbar";
 import Markdown from "markdown-to-jsx";
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 
 import { content } from "./accounts.help";
+import { useAuth, type account } from "../helpers/authn";
 
 function Account() {
+  const { getAccounts, addAccount } = useAuth();
   const [help, setHelp] = useState(false);
+  const [accounts, setAccounts] = useState([] as account[]);
+
+  useEffect(() => {
+    getAccounts().then((accounts) => {
+      setAccounts(accounts);
+    });
+  }, []);
+
+  const [selectedAccountNumber, setSelectedAccountNumber] = useState(0);
+
+  const handleOnChange = (value: string) => {
+    setSelectedAccountNumber(parseInt(value));
+  };
+
+  const add = async () => {
+    const account = await addAccount();
+    if (account) {
+      setAccounts([...accounts, account]);
+    }
+  };
+
   return (
     <>
       <NavBar />
-      <button onClick={() => {
+      <button
+        onClick={() => {
           setHelp(!help);
-        }}>
+        }}
+      >
         {help ? "Hide help" : "Show help"}
       </button>
       <h1>Networks and Accounts</h1>
       {help ? (
         <Markdown>{content}</Markdown>
       ) : (
-        "Oops! This page is under construction."
+        <>
+          <button onClick={add}>Add account</button>
+          {accounts.length > 0 && selectedAccountNumber < accounts.length ? (
+            <>
+              <select
+                onChange={(e) => handleOnChange(e.currentTarget.value)}
+                value={selectedAccountNumber}
+              >
+                {accounts.map((a: account, index: number) => (
+                  <option key={index} value={index}>
+                    {a.name}
+                  </option>
+                ))}
+              </select>
+              <div>
+                <p>Address: {accounts[selectedAccountNumber].address}</p>
+                <p>Public key: {accounts[selectedAccountNumber].publickey}</p>
+              </div>
+            </>
+          ) : (
+            <div>No Accounts</div>
+          )}
+        </>
       )}
     </>
   );
