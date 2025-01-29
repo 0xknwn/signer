a199021649d34dd802965659a46672ca7fd42584
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-01-29 10:32:54 +0100

    create a proxy to the devnet

--- localonly/apis.ts
+++ localonly/apis.ts
@@ -1,6 +1,8 @@
 import { apiT } from "./plugin";
 
 import version from "../_api/version";
+import rpc from "./rpc";
+
 const apis: apiT[] = [{ route: "version", handler: version }];
 
 export default apis;
