7d4804b75cdc7457a637948f032824fb15f5b871
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-01-31 21:35:33 +0100

    attempt to run response

--- _api/version.ts
+++ _api/version.ts
@@ -1,16 +1,10 @@
 // file: _api/version.ts
+import { geolocation } from "@vercel/edge";
 
 // Should run on edge runtime
 export const edge = true;
 
-// Stream the response
-export const streaming = true;
-
-// Enable Incremental Static Regeneration for this endpoint
-// export const isr = {
-//   expiration: 30,
-// };
-
-export default async function handler(req, res) {
-  res.send({version: "dev" })
+export default function handler(req: Request) {
+  const { city } = geolocation(req);
+  return new Response(`{"version": "dev", "city": "${city}"}`);
 }
