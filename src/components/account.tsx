0ecbc7ae0d1f6f3a8b455dfa5b9c00920bbca4be
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-01-31 02:44:53 +0100

    manage the account deployment

---
+++ src/components/account.tsx
@@ -0,0 +1,59 @@
+import { useAccounts } from "../helpers/accounts";
+import Jazzicon, { jsNumberForAddress } from "react-jazzicon";
+
+const Account = () => {
+  const { accounts, selectedAccountNumber } = useAccounts();
+
+  return (
+    <>
+      {accounts.length > 0 && selectedAccountNumber < accounts.length ? (
+        <>
+          <div>
+            Address:{" "}
+            <Jazzicon
+              diameter={32}
+              seed={jsNumberForAddress(accounts[selectedAccountNumber].address)}
+            />{" "}
+            {accounts[selectedAccountNumber].address.substring(0, 6) +
+              "..." +
+              accounts[selectedAccountNumber].address.substring(
+                accounts[selectedAccountNumber].address.length - 4,
+                accounts[selectedAccountNumber].address.length
+              )}
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  accounts[selectedAccountNumber].address
+                );
+              }}
+            >
+              Copy
+            </button>
+          </div>
+          <div>
+            Public key:{" "}
+            {accounts[selectedAccountNumber].publickey.substring(0, 6) +
+              "..." +
+              accounts[selectedAccountNumber].publickey.substring(
+                accounts[selectedAccountNumber].publickey.length - 4,
+                accounts[selectedAccountNumber].publickey.length
+              )}
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  accounts[selectedAccountNumber].publickey
+                );
+              }}
+            >
+              Copy
+            </button>
+          </div>
+        </>
+      ) : (
+        <p>No accounts Selected</p>
+      )}
+    </>
+  );
+};
+
+export default Account;
