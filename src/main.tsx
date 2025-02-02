3da25cc6d6e3cc258bbbaaa12fa10d7fd55ee7dc
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-02 19:15:06 +0100

    remove .tsx from import (#57)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import { App } from "./app.tsx";
+import { App } from "./app";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
