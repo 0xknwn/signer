f0793e096d91c53130ce99f20d83960380e5b5ab
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-14 11:37:19 +0100

    remove the message server from dev

--- localonly/server.ts
+++ localonly/server.ts
@@ -17,33 +17,6 @@ app.get(
   }
 );
 
-const pipe = [] as string[];
-
-app.post(
-  "/message",
-  async (request: ExpressRequest, response: ExpessResponse) => {
-    const req = new Request("https://example.com");
-    const data = [] as string[];
-    request.on("data", (chunk) => {
-      data.push(chunk.toString());
-    });
-    request.on("end", () => {
-      pipe.push(data.join(""));
-    });
-    const res = handler(req);
-    response.status(200).send({ status: "OK" });
-  }
-);
-
-app.get("/message", async (_: ExpressRequest, response: ExpessResponse) => {
-  const out = pipe.shift();
-  if (!out) {
-    response.status(200).send(`{}`);
-    return;
-  }
-  response.status(200).send(out);
-});
-
 app
   .listen(PORT, () => {
     console.log("Server running at PORT: ", PORT);
