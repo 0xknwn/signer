9084e8cacad576cfcb8705fde3ea985a0c51eb32
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-14 02:42:45 +0100

    add an application tab to notifier

---
+++ src/routes/notifier/applications.tsx
@@ -0,0 +1,11 @@
+import NotificationList from "../../components/notification_list";
+
+function Messages() {
+  return (
+    <>
+      <h2>Connect to an Application</h2>
+    </>
+  );
+}
+
+export default Messages;
