4188d06ce45b759d1f2a5a0474cad57f73442208
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-14 16:19:28 +0100

    accept channel and query messages

--- src/routes/accounts/applications.tsx
+++ src/routes/accounts/applications.tsx
@@ -3,23 +3,69 @@ import {
   acknowledgeChannelRequest,
   acknowledgeChannelRequestResult,
   acknowledgeChannelRequestParams,
-  // acceptChannel,
-  // acceptChannelResult,
-  // acceptChannelParams,
+  acceptChannel,
+  exportPublicKeyToHex,
+  acceptChannelParams,
+  encryptAndSign,
+  acceptChannelUniqueKeys,
   generateEncryptionKey,
   generateChannelID,
   channelRequestUniqueKeys,
+  channelUniqueKeys,
+  queryMessagesResult,
+  queryMessages,
+  verify,
+  hex2buf,
 } from "@0xknwn/connect-api";
 
 const baseURL = import.meta.env.VITE_API_URL || "/api";
 
+// @todo: replace with actual account address
+const accountAddress = "0x1";
+const generateKey = async (namedCurve = "P-256") => {
+  const { publicKey, privateKey } = await window.crypto.subtle.generateKey(
+    {
+      name: "ECDSA",
+      namedCurve,
+    },
+    true,
+    ["sign", "verify"]
+  );
+  return { publicKey, privateKey };
+};
+
+const generateECDHKey = async (namedCurve = "P-256") => {
+  const { publicKey, privateKey } = await window.crypto.subtle.generateKey(
+    {
+      name: "ECDH",
+      namedCurve,
+    },
+    true,
+    ["deriveKey", "deriveBits"]
+  );
+  return { publicKey, privateKey };
+};
+
+const importVerifyinghKeyFromHex = async (key: string) => {
+  const rawKey = hex2buf(key);
+  return window.crypto.subtle.importKey(
+    "raw",
+    rawKey,
+    { name: "ECDSA", namedCurve: "P-256" },
+    true,
+    ["verify"]
+  );
+};
+
 enum ConnectionState {
   Initializing = "initializing",
   NoRequests = "norequests",
-  APIError = "apierror",
+  APIRequestError = "apirequesterror",
   RequestAcknowledged = "requestacknowledged",
   RequestAccepted = "requestaccepted",
+  APIAcceptError = "apiaccepterror",
   ChannelOpened = "channelopened",
+  ChannelExpired = "channelexpired",
 }
 
 type SignerProps = {
@@ -35,6 +81,8 @@ type CallbackProps = {
   setApp: (app: acknowledgeChannelRequestResult | null) => void;
   setSigner: (signer: SignerProps | null) => void;
   setChannelID: (channelID: string) => void;
+  setReceivedMessages: (messages: { [nonce: string]: boolean }) => void;
+  setMessages: (messages: any[]) => void;
 };
 
 const connect = async (pin: string, callback: CallbackProps) => {
@@ -45,7 +93,7 @@ const connect = async (pin: string, callback: CallbackProps) => {
   } as acknowledgeChannelRequestParams);
   if (!response.ok) {
     console.error("Cannot connect to the API: ", response.status);
-    setConnectionState(ConnectionState.APIError);
+    setConnectionState(ConnectionState.APIRequestError);
     setPin("");
     return;
   }
@@ -57,16 +105,18 @@ const connect = async (pin: string, callback: CallbackProps) => {
   }
   if (result.error) {
     console.error("API Error: ", result.error);
-    setConnectionState(ConnectionState.APIError);
+    setConnectionState(ConnectionState.APIRequestError);
     setPin("");
     return;
   }
+  console.log("Request acknowledged");
   setConnectionState(ConnectionState.RequestAcknowledged);
   setApp(result.result as acknowledgeChannelRequestResult);
 };
 
 const accept = async (
   app: acknowledgeChannelRequestResult | null,
+  pin6: string,
   callback: CallbackProps
 ) => {
   if (!app || !app.agentEncryptionPublicKey) {
@@ -74,47 +124,118 @@ const accept = async (
     return;
   }
   const { setPin4, setConnectionState, setChannelID, setSigner } = callback;
-  const code4 = Math.floor(Math.random() * 1000)
+  const pin4 = Math.floor(Math.random() * 1000)
     .toString()
     .padStart(4, "0");
-  setPin4(code4);
+  setPin4(pin4);
   setConnectionState(ConnectionState.RequestAccepted);
+  const { publicKey: identityPublicKey, privateKey: identityPrivateKey } =
+    await generateKey();
+  const { publicKey: sharingPublicKey, privateKey: sharingPrivateKey } =
+    await generateECDHKey();
   const channelID = generateChannelID();
-  setChannelID(channelID);
-  const sharingKey = await window.crypto.subtle.generateKey(
-    {
-      name: "ECDH",
-      namedCurve: "P-256",
-    },
-    true,
-    ["deriveKey", "deriveBits"]
-  );
-  const signingKey = await window.crypto.subtle.generateKey(
-    {
-      name: "ECDSA",
-      namedCurve: "P-256",
-    },
-    true,
-    ["sign", "verify"]
-  );
   const encryptionKey = await generateEncryptionKey(
-    sharingKey.privateKey,
-    app?.agentEncryptionPublicKey
+    sharingPrivateKey,
+    app.agentEncryptionPublicKey
   );
-  setSigner({
-    identityKey: signingKey,
+  const { encryptedMessage, signature } = await encryptAndSign(
     encryptionKey,
-    sharingKey,
+    identityPrivateKey,
+    channelID
+  );
+  const ackChannelKeys = await acceptChannelUniqueKeys(
+    pin6,
+    pin4,
+    app.relyingParty,
+    app.signerAccountID,
+    app.deadline
+  );
+  const acceptChannelResult = await acceptChannel(baseURL, 2, {
+    acceptChannelUniqueKeys: ackChannelKeys,
+    signerPublicKey: await exportPublicKeyToHex(identityPublicKey),
+    signerEncryptionPublicKey: await exportPublicKeyToHex(sharingPublicKey),
+    signerAccountAddress: accountAddress,
+    encryptedChannelIdentifier: encryptedMessage,
+    channelIdentifierSignature: signature,
+    deadline: app.deadline,
+  } as acceptChannelParams);
+  if (acceptChannelResult.ok) {
+    console.log("Channel accepted");
+  }
+  const output = await acceptChannelResult.json();
+  if (output.error) {
+    console.error(output.error);
+    return;
+  }
+  setChannelID(channelID);
+  setSigner({
+    identityKey: {
+      publicKey: identityPublicKey,
+      privateKey: identityPrivateKey,
+    },
+    encryptionKey,
+    sharingKey: { publicKey: sharingPublicKey, privateKey: sharingPrivateKey },
   });
-  // const response = await acceptChannel(baseURL, 1, {
-  //   channelID,
-  //   encryptionKey,
-  //   sharingKey,
-  //   signingKey,
-  // } as acceptChannelParams);
 };
 
-function Messages() {
+// @todo: check why we do not receive multiple messages when there are several
+// pending messages.
+// @todo: automate the query every 5 secs instead of the button click
+const query = async (
+  app: acknowledgeChannelRequestResult,
+  channelID: string,
+  receivedMessages: { [nonce: string]: boolean },
+  messages: any[],
+  callback: CallbackProps
+) => {
+  if (Math.floor(Date.now() / 1000) >= app.deadline) {
+    console.log("Deadline reached, channel has expired");
+    const { setConnectionState } = callback;
+    setConnectionState(ConnectionState.ChannelExpired);
+    return;
+  }
+  const keys = await channelUniqueKeys(app.relyingParty, channelID);
+  const response = await queryMessages(baseURL, 3, {
+    channelUniqueKeys: keys,
+  });
+  if (!response.ok) {
+    console.error(await response.json());
+    throw new Error("Unexpected Error calling API");
+  }
+  const payload = await response.json();
+  if (payload.error && payload.error.code === -32003) {
+    return [];
+  }
+  if (payload.error) {
+    throw new Error("Unexpected Error calling API" + payload.error);
+  }
+  const result = payload.result as queryMessagesResult;
+  const { setReceivedMessages, setMessages } = callback;
+  if (Array.isArray(result.messages)) {
+    const verifyingKey = await importVerifyinghKeyFromHex(app.agentPublicKey);
+    for (let [idx, _] of result.messages.entries()) {
+      const message = result.messages[idx];
+      const signature = result.messageSignatures[idx];
+      const verified = await verify(verifyingKey, message, signature);
+      const jsonMessage = JSON.parse(message);
+      console.log(
+        "receivedMessages",
+        jsonMessage.nonce,
+        receivedMessages[jsonMessage.nonce]
+      );
+      if (receivedMessages[jsonMessage.nonce]) {
+        continue;
+      }
+      setReceivedMessages({ ...receivedMessages, [jsonMessage.nonce]: true });
+      if (!verified) {
+        console.error("Message not verified");
+      }
+      setMessages([...messages, message]);
+    }
+  }
+};
+
+const Messages = () => {
   const [pin, setPin] = useState("");
   const [pin4, setPin4] = useState("");
   const [app, setApp] = useState({} as acknowledgeChannelRequestResult | null);
@@ -123,6 +244,10 @@ function Messages() {
   const [connectionState, setConnectionState] = useState(
     ConnectionState.Initializing
   );
+  const [receivedMessages, setReceivedMessages] = useState(
+    {} as { [nonce: string]: boolean }
+  );
+  const [messages, setMessages] = useState([] as any[]);
   const callback = {
     setConnectionState,
     setPin,
@@ -130,17 +255,38 @@ function Messages() {
     setPin4,
     setSigner,
     setChannelID,
+    setReceivedMessages,
+    setMessages,
   };
+
   useEffect(() => {
+    console.log("=== useEffect ===");
+  });
+
+  useEffect(() => {
+    console.log("=== useEffect ===");
     if (connectionState === ConnectionState.Initializing) {
+      console.log("=== 0 ===");
       return;
     }
     if (connectionState === ConnectionState.NoRequests) {
-      setInterval(() => {
-        console.log("Polling for messages");
+      console.log("=== 1 ===");
+      const interval = setInterval(() => {
         setConnectionState(ConnectionState.Initializing);
       }, 3000);
-      return;
+      return () => clearInterval(interval);
+    }
+    if (connectionState === ConnectionState.APIAcceptError) {
+      console.log("=== 2 ===");
+      const interval = setInterval(() => {
+        setConnectionState(ConnectionState.Initializing);
+        setApp(null);
+        setSigner(null);
+        setChannelID("");
+        setPin("");
+        setPin4("");
+      }, 3000);
+      return () => clearInterval(interval);
     }
     return;
   }, [connectionState]);
@@ -162,7 +308,7 @@ function Messages() {
         </>
       ) : connectionState === ConnectionState.NoRequests ? (
         <p>No request found...</p>
-      ) : connectionState === ConnectionState.APIError ? (
+      ) : connectionState === ConnectionState.APIRequestError ? (
         <p>Error calling API...</p>
       ) : connectionState === ConnectionState.RequestAcknowledged ? (
         <>
@@ -183,20 +329,33 @@ function Messages() {
           >
             Disconnect
           </button>
-          <button onClick={() => accept(app, callback)}>Accept</button>
+          <button onClick={() => accept(app, pin, callback)}>Accept</button>
         </>
       ) : connectionState === ConnectionState.RequestAccepted ? (
         <>
           <p>Request Accepted, use {pin4} on the agent to open the channel</p>
           <button
             onClick={() => {
-              setConnectionState(ConnectionState.ChannelOpened);
+              setConnectionState(ConnectionState.Initializing);
               setPin("");
               setPin4("");
+              setApp(null);
+              setSigner(null);
             }}
           >
-            OK
+            Reset
           </button>
+          <button
+            onClick={() => {
+              setConnectionState(ConnectionState.ChannelOpened);
+            }}
+          >
+            Connect
+          </button>
+        </>
+      ) : connectionState === ConnectionState.APIAcceptError ? (
+        <>
+          <p>Error accepting Channel...</p>
         </>
       ) : connectionState === ConnectionState.ChannelOpened ? (
         <>
@@ -211,12 +370,47 @@ function Messages() {
           >
             Reset
           </button>
+          <button
+            onClick={() => {
+              if (!app) {
+                console.log("no app");
+                return;
+              }
+              query(
+                app as acknowledgeChannelRequestResult,
+                channelID,
+                receivedMessages,
+                messages,
+                callback
+              );
+            }}
+          >
+            Receive
+          </button>
+          <ul>
+            {messages.map((msg, idx) => (
+              <li key={idx}>{JSON.stringify(msg)}</li>
+            ))}
+          </ul>
+        </>
+      ) : connectionState === ConnectionState.ChannelExpired ? (
+        <>
+          <p>Channel has expired...</p>
+          <button
+            onClick={() => {
+              setConnectionState(ConnectionState.Initializing);
+              setPin("");
+              setPin4("");
+            }}
+          >
+            Reconnect
+          </button>
         </>
       ) : (
         <p>Stat not managed yet</p>
       )}
     </>
   );
-}
+};
 
 export default Messages;
